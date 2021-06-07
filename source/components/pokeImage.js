03a395db3ab18f3fbc7620d8a7d9dfc526832561
gdys92 <gdys92@gmail.com>
2021-06-07 16:12:08 -0300

    new

--- source/components/pokeImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class PokeImage extends React.Component{
-	render(){
-		return <figure className="PokeDetail-image">
-           	 <img src={`images/pokemons/${this.props.pkmName}.jpg`}/>
-           </figure>
-	}
-}
