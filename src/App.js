3d01b7cc4a24a9a24773d03e6773bbda345cdcf2
gdys92 <gdys92@gmail.com>
2021-06-08 11:21:38 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
    
+   <h1>Pokedex</h1>
+   <div className="pokemon-container">
+     <div className="all-container">
+     </div>
+     
+     <button className="load-more">Load more</button>
+   </div>
    
-    </div>
+   </div>
   );
 }
 
