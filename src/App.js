f77cc0b4d74846c8ecd6c84c14d6ac4461e26b24
gdys92 <gdys92@gmail.com>
2021-06-10 13:01:05 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import PokemonThumb from './components/PokemonThumb'
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => {
+import "./App.css";
+import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/layout/Dashboard";
+import Pokemon from "./components/pokemon/Pokemon";
 
-   const[allPokemons, setAllPokemons] = useState([])
-   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+import backgroundImage from "./image.jpg";
 
-  const getAllPokemons = async () => {
-    const res = await fetch(loadMore)
-    const data = await res.json()
-
-    setLoadMore(data.next)
-
-    function createPokemonObject(results)  {
-      results.forEach( async pokemon => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data =  await res.json()
-        setAllPokemons( currentList => [...currentList, data])
-        await allPokemons.sort((a, b) => a.id - b.id)
-      })
-    }
-    createPokemonObject(data.results)
-  }
-
- useEffect(() => {
-  getAllPokemons()
- }, [])
-
-  return (
-    <div className="app-container">
-      <h1>Pokedex</h1>
-      <div className="pokemon-container">
-        <div className="all-container">
-          {allPokemons.map( (pokemonStats, index) => 
-            <PokemonThumb
-              key={index}
-              id={pokemonStats.id}
-              image={pokemonStats.sprites.other.dream_world.front_default}
-              name={pokemonStats.name}
-              type={pokemonStats.types[0].type.name}
-            />)}
-          
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" style={{ background: `url(${backgroundImage})` }}>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
         </div>
-          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
-      </div>
-    </div>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
